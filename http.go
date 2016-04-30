f49213a32667207874bc15db27f0b08df3baad49
Rui <rliu054@uottawa.ca>
2016-04-29 23:02:46 -0400

    Work so far. Testing with VSC

--- http.go
+++ http.go
@@ -1,13 +1,24 @@
 package main
 
 import (
-    // "time"
-    "flag"
+	"flag"
+	"log"
+	"net/http"
 )
 
 var (
-    mirror = flag.String("mirror", "", "Mirror web base URL")
-    logfile = flag.String("log", "-", "Set log file, default stdout")
-    upstream = flag.String("upstream", "", "server base URL, conflict with -mirror")
+	mirror   = flag.String("mirror", "", "Mirror web base URL")
+	logfile  = flag.String("log", "-", "Set log file, default stdout")
+	upstream = flag.String("upstream", "", "server base URL, conflict with -mirror")
+	address  = flag.String("addr", ":5000", "Listen address")
+	token    = flag.String("token", "", "peer and master token should be same")
 )
 
+func main() {
+	flag.Parse()
+	http.HandleFunc("/", FileHandler)
+	http.HandleFunc("/_log", LogHandler)
+
+	log.Printf("Listening on %s", *address)
+	log.Fatal(http.ListenAndServe(*address))
+}
